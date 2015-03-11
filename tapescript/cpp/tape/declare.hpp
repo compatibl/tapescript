498a00cb129a18e7ab652c9417799ce9dee2bfcd
CompatibL <team@compatibl.com>
2015-03-11 19:07:16 -0400

    Added import, declare, implement headers.

---
+++ tapescript/cpp/tape/declare.hpp
@@ -0,0 +1,32 @@
+﻿/*
+Copyright (C) 2003-2015 CompatibL
+
+This file is part of TapeScript, an open source library and tape encoding
+standard for adjoint algorithmic differentiation (AAD), available from
+
+    http://github.com/compatibl/tapescript (source)
+    http://modval.org/tapescript (documentation)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef __tape_declare_hpp__
+#define __tape_declare_hpp__
+
+#include <tape/import.hpp>
+
+#ifndef TAPE_API
+#   define TAPE_API TAPE_DECLARE
+#endif
+
+#endif // __tape_declare_hpp__
---
+++ tapescript/cpp/tape/import.hpp
@@ -0,0 +1,61 @@
+﻿/*
+Copyright (C) 2003-2015 CompatibL
+
+This file is part of TapeScript, an open source library and tape encoding
+standard for adjoint algorithmic differentiation (AAD), available from
+
+    http://github.com/compatibl/tapescript (source)
+    http://modval.org/tapescript (documentation)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef __tape_import_hpp__
+#define __tape_import_hpp__
+
+// Supports compilation into Windows DLL
+#ifndef TAPE_IMPLEMENT
+#   if defined(_WIN32) && !defined(__MINGW32__)
+#       define TAPE_IMPLEMENT __declspec(dllexport)
+#       define TAPE_DECLARE __declspec(dllimport)
+#   else
+#       define TAPE_IMPLEMENT
+#       define TAPE_DECLARE
+#   endif
+#endif
+
+// Avoid problems with macros min/max on windows(windows.h)
+#define NOMINMAX
+
+#ifdef WIN32
+// Avoid compiler warning about DLL export for STL types 
+#   pragma warning(disable : 4251)
+// Avoid compiler warning about conversion from size_t to int
+#   pragma warning(disable : 4267)
+#endif
+
+// Standard STL includes
+#include <string>
+#include <vector>
+#include <set>
+#include <memory>
+#include <limits>
+#include <stdexcept>
+#include <map>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+
+#endif // __tape_import_hpp__
