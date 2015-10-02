1cbc442b7d0db7695c2af704213d5106b24cb057
CompatibL <team@compatibl.com>
2015-10-01 22:54:21 -0400

    Corrected formatting.

--- tapescript/cpp/cl/tape/impl/base_inner_vector.hpp
+++ tapescript/cpp/cl/tape/impl/base_inner_vector.hpp
@@ -205,34 +205,34 @@ namespace CppAD
     
         return returnValue;
     }
-}
 
-namespace CppAD {
 	CPPAD_COND_EXP_REL(cl::InnerVector)
-}
 
-//namespace CppAD {
 //	inline bool EqualOpSeq(const cl::InnerVector& x, const cl::InnerVector& y)
 //	{	return x == y; }
-//}
-//
-namespace CppAD {
+
 	inline bool IdenticalPar(const cl::InnerVector& x)
-	{	return true; }
+	{
+	    return true;
+	}
 	inline bool IdenticalZero(const cl::InnerVector& x)
-	{	return (x == 0.); }
+	{
+	    return x == 0.0;
+	}
 	inline bool IdenticalOne(const cl::InnerVector& x)
-	{	return (x == 1.); }
+	{
+	    return x == 1.0;
+	}
 	inline bool IdenticalEqualPar(const cl::InnerVector& x, const cl::InnerVector& y)
-	{	return (x == y); }
-}
+	{
+	    return (x == y);
+	}
 
-namespace CppAD {
 	inline int Integer(const cl::InnerVector& x)
-	{	return static_cast<int>(x.to_scalar()); }
-}
+	{
+	    return static_cast<int>(x.to_scalar());
+	}
 
-namespace CppAD {
 	inline bool GreaterThanZero(const cl::InnerVector& x)
 	{	return x > 0.; }
 	inline bool GreaterThanOrZero(const cl::InnerVector& x)
@@ -243,9 +243,7 @@ namespace CppAD {
 	{	return x <= 0.; }
 	inline bool abs_geq(const cl::InnerVector& x, const cl::InnerVector& y)
 	{	return std::abs(x) >= std::abs(y); }
-}
 
-namespace CppAD {
 	CPPAD_STANDARD_MATH_UNARY(cl::InnerVector, acos)
 	CPPAD_STANDARD_MATH_UNARY(cl::InnerVector, asin)
 	CPPAD_STANDARD_MATH_UNARY(cl::InnerVector, atan)
@@ -263,14 +261,12 @@ namespace CppAD {
 # if CPPAD_COMPILER_HAS_ERF
 	CPPAD_STANDARD_MATH_UNARY(cl::InnerVector, erf)
 # endif
-}
 
-namespace CppAD {
 	inline cl::InnerVector abs(const cl::InnerVector& x)
-	{	return std::fabs(x); }
-}
-
-namespace CppAD {
+    {
+        return std::fabs(x);
+    }
+    
 	inline cl::InnerVector sign(const cl::InnerVector& x)
 	{
         auto sign_func = [](double v)
@@ -284,31 +280,38 @@ namespace CppAD {
 
         return x.apply(sign_func);
 	}
-}
 
-namespace CppAD {
 	inline cl::InnerVector pow(const cl::InnerVector& x, const cl::InnerVector& y)
-	{ return std::pow(x, y); }
-}
+	{
+	    return std::pow(x, y);
+	}
 
-namespace CppAD {
 	template <>
-	class numeric_limits<cl::InnerVector> {
+	class numeric_limits<cl::InnerVector>
+    {
 	public:
 		// machine epsilon
 		static cl::InnerVector epsilon(void)
-		{	return std::numeric_limits<cl::InnerVector>::epsilon(); }
+		{
+		    return std::numeric_limits<cl::InnerVector>::epsilon();
+		}
 		// minimum positive normalized value
 		static cl::InnerVector min(void)
-		{	return std::numeric_limits<cl::InnerVector>::min(); }
+		{
+		    return std::numeric_limits<cl::InnerVector>::min();
+		}
 		// maximum finite value
 		static cl::InnerVector max(void)
-		{	return std::numeric_limits<cl::InnerVector>::max(); }
+		{
+		    return std::numeric_limits<cl::InnerVector>::max();
+		}
 	};
 	// deprecated machine epsilon
 	template <> 
 	inline cl::InnerVector epsilon<cl::InnerVector>(void)
-	{	return numeric_limits<cl::InnerVector>::epsilon(); }
+	{
+        return numeric_limits<cl::InnerVector>::epsilon();
+    }
 }
 
 
