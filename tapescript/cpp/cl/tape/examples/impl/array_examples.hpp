47c02faf0288452342df0712d3368b995344687b
CompatibL <team@compatibl.com>
2015-10-03 07:52:25 -0400

    Added reverse mode trace to example.

--- tapescript/cpp/cl/tape/examples/impl/array_examples.hpp
+++ tapescript/cpp/cl/tape/examples/impl/array_examples.hpp
@@ -62,11 +62,7 @@ namespace cl
         out_str << "Reverse(1, w) sweep for w = " << w << "..." << std::endl;
         std::vector<cl::InnerArray> rev = f.Reverse(1, w);
 
-        std::ofstream of("output_reverse.txt");
-        CppAD::tape_serializer<cl::InnerArray> serializer(of);
-        serializer.precision(3);
-
-
+        CppAD::tape_serializer<cl::InnerArray>& serializer(static_cast<CppAD::tape_serializer<cl::InnerArray>&>(out_str));
         std::vector<cl::InnerArray> const& rev_ = f.Reverse(1, std::make_pair(w, &serializer)).first;
         out_str << "Reverse sweep result: " << rev << "\n\n\n";
     }
